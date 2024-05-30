f959f1c8a1351f82ab1f0be7d08b9479b80a367f
Cristian Peña <48634410+cristianpenia@users.noreply.github.com>
2024-05-30 14:49:40 -0600

    initial commit

---
+++ SwiftUIBeta/SwiftUIBeta/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftUIBeta
+//
+//  Created by Cristian Peña Barrios on 30/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+            Text("King Rufus!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIBeta/SwiftUIBeta/SwiftUIBetaApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIBetaApp.swift
+//  SwiftUIBeta
+//
+//  Created by Cristian Peña Barrios on 30/05/24.
+//
+
+import SwiftUI
+
+// indica cual es la primer pantalla al correr la app
+@main
+struct SwiftUIBetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
