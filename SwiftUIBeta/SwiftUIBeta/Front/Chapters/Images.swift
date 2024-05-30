b93e40220b6109a2ad5d24f58b9bdea74fd78a3e
Cristian Peña <48634410+cristianpenia@users.noreply.github.com>
2024-05-30 15:14:49 -0600

    reoganize the project

---
+++ SwiftUIBeta/SwiftUIBeta/Front/Chapters/Images.swift
@@ -0,0 +1,18 @@
+//
+//  Images.swift
+//  SwiftUIBeta
+//
+//  Created by Cristian Peña Barrios on 30/05/24.
+//
+
+import SwiftUI
+
+struct Images: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Images()
+}
