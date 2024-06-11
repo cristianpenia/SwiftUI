01c0ffe8420538c1c816488f9d10ec4f40688f1b
Cristian Peña <48634410+cristianpenia@users.noreply.github.com>
2024-06-11 15:41:54 -0600

    chapter 23 - FullScreen and Sheet

---
+++ SwiftUIBeta/SwiftUIBeta/Front/Chapters/ExampleFullScreenCoverAndSheet.swift
@@ -0,0 +1,45 @@
+//
+//  ExampleFullScreenCoverAndSheet.swift
+//  SwiftUIBeta
+//
+//  Created by Cristian Peña Barrios on 11/06/24.
+//
+
+import SwiftUI
+
+struct ExampleFullScreenCoverAndSheet: View {
+    
+    @State var isPresented: Bool = false
+    
+    var body: some View {
+        
+        VStack {
+            Text("View 1")
+                .padding()
+            
+            Button {
+                isPresented = true
+            } label: {
+                Text("OK")
+            }
+        }
+        .sheet(isPresented: $isPresented,
+//        .fullScreenCover(isPresented: $isPresented,
+                         onDismiss: { isPresented = false },
+                         content: {
+            ZStack {
+                Color.red.ignoresSafeArea()
+                Button {
+                    isPresented = false
+                } label: {
+                    Text("Hola Rufus!")
+                }
+
+            }
+        })
+    }
+}
+
+#Preview {
+    ExampleFullScreenCoverAndSheet()
+}
