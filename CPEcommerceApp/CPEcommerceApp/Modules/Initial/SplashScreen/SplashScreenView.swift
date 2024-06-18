34b88f1b2ff8d8865e90e3f4ca3b3f9ca691298d
Cristian Peña <48634410+cristianpenia@users.noreply.github.com>
2024-06-18 16:24:32 -0600

    [ADD] colors

--- CPEcommerceApp/CPEcommerceApp/Modules/Initial/SplashScreen/SplashScreenView.swift
+++ CPEcommerceApp/CPEcommerceApp/Modules/Initial/SplashScreen/SplashScreenView.swift
@@ -14,6 +14,7 @@ struct SplashScreenView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Splash Screen")
+                .foregroundStyle(.ecommerce)
         }
         .padding()
     }
