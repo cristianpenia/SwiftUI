0c229472b6766b8b2ec83c5b151d49b5003291cd
Cristian Peña <48634410+cristianpenia@users.noreply.github.com>
2024-06-18 16:15:40 -0600

    [ADD] icon and color

--- CPEcommerceApp/CPEcommerceApp/Modules/Initial/SplashScreen/SplashScreenView.swift
+++ CPEcommerceApp/CPEcommerceApp/Modules/Initial/SplashScreen/SplashScreenView.swift
@@ -7,18 +7,18 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct SplashScreenView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Splash Screen")
         }
         .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    SplashScreenView()
 }
