5be308d56794e3dfe6668557cd53e0df1e7d7229
Cristian Peña <48634410+cristianpenia@users.noreply.github.com>
2024-06-18 16:04:40 -0600

    [ADD] project

---
+++ CPEcommerceApp/CPEcommerceApp/Modules/SplashScreen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CPEcommerceApp
+//
+//  Created by Cristian Peña Barrios on 18/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
