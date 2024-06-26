29e97611de0236f0ca74b7bf4057761015e83fe9
Cristian Peña <48634410+cristianpenia@users.noreply.github.com>
2024-06-25 18:38:07 -0600

    [ADD] initial commit

---
+++ CPCalculatoraApp/CPCalculatoraApp/Application/CPCalculatoraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CPCalculatoraAppApp.swift
+//  CPCalculatoraApp
+//
+//  Created by Cristian Peña Barrios on 25/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CPCalculatoraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
